742698fe0d845cc2d285e3c01618a75534691b35
Abel Watson <80369480+abelwatson@users.noreply.github.com>
2021-06-24 17:54:38 -0400

    eleven journal complete

--- client/user-scripts.js
+++ client/user-scripts.js
@@ -2,10 +2,11 @@
 *** USER SIGNUP ***
 ************************** */
 function userSignUp() {
-    console.log('userSignUp Function Called')
+    //console.log('userSignUp Function Called')
 
     let userEmail = document.getElementById("emailSignup").value;
     let userPass = document.getElementById("pwdSignup").value;
+    // console.log(userEmail, userPass)
 
     let newUserData = {
         user: {
@@ -13,6 +14,7 @@ function userSignUp() {
             password: userPass
         }
     };
+    console.log(userData)
 
     console.log(`newUserData --> ${newUserData.user.email} ${newUserData.user.password}`);
 
@@ -27,35 +29,81 @@ function userSignUp() {
         .then(data => {
             console.log(data);
             let token = data.sessionToken;
-            localStorage.setItem('SessionToken', token);
+            localStorage.setItem(`SessionToken`, token);
             tokenChecker();
         })
         .catch(err => {
         console.error(err)
         })
 };
-    
-    
     /* *************************
     *** USER LOGIN ***
     ************************** */
-    function userLogin() {
-     console.log('userLogin Function Called')
-    }
+function userLogin() {
+    let userEmail = document.getElementById("emailLogin").value;
+    let userPass = document.getElementById("pwdLogin").value;
+    console.log(userEmail, userPass)
+
+    let userData = {
+        user: {
+            email: userEmail,
+            password: userPass
+        }
+    };
+    console.log(userData)
+
+    fetch(`http://localhost:3000/user/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let token = data.sessionToken;
+            localStorage.setItem(`SessionToken`, token);
+            tokenChecker();
+        })
+        .catch(err => {
+        console.error(err)
+        })
+}
     
     
     /* *************************
     *** USER LOGOUT ***
     ************************** */
-    function userLogout() {
-     console.log('userLogout Function Called')
-    }
+function userLogout() {
+
+    localStorage.setItem('SessionToken', undefined);
+    console.log(`SessionToken --> ${localStorage.SessionToken}`);
+    tokenChecker();
+    console.log('userLogout Function Called')
+}
     
     
     /* *************************
      *** TOKEN CHECKER FUNCTION ***
     ************************** */
-    function tokenChecker() {
-     console.log('tokenChecker Function Called')
+function tokenChecker() {
+    let display = document.getElementById('journals');
+    let header = document.createElement('h5');
+    let accessToken = localStorage.getItem('SessionToken');
+    let alertText = "Log in or sign up to get started!";
+
+    for (let i = 0; i < display.childNodes.length; i++) {
+        display.removeChild(display.firstChild);
     }
-    tokenChecker()
+
+    if (accessToken === 'undefined') {
+        display.appendChild(header);
+        header.textContent = alertText;
+        header.setAttribute('id', 'defaultLogin');
+    } else {
+        null
+    }
+}
+
+tokenChecker()
